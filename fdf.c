49db21c76ec155a8db2982ac64dfd7e5f41ae179
Anton Sadik <asadik@c3r11p8.42vienna.com>
2026-02-09 14:42:18 +0100

    laying out general structure

--- fdf.c
+++ fdf.c
@@ -6,6 +6,50 @@
 #include "libft/libft.h"
 #include "fdf.h"
 
+int	setup_world(const char *file, t_world *world)
+{
+	int			fd;
+	char		*line;
+	
+	fd = open(file, O_RDONLY);
+	line = get_next_line_single(fd);
+}
+
+t_state	setup(const char *file)
+{
+	t_state		state;
+	state.mlx.mlx_ptr = mlx_init();
+	state.mlx.win_ptr = mlx_new_window(state.mlx.mlx_ptr, 500, 500, "FDF");
+	state.image.img = mlx_new_image(state.mlx.mlx_ptr, 500, 500);
+	state.image.addr = mlx_get_data_addr(state.image.img, &state.image.bits_per_pixel,
+		&state.image.line_length,&state.image.endian);
+	state.mlx.img = state.image;
+	setup_world(file, &state.world);
+}
+
+int	main(int varc, char **argv) {
+	t_state		state;
+	t_image		img;
+	t_mlx_data	mlx;
+
+
+	if (varc != 2)
+		return (0);
+	state = setup(argv[1]);
+	while (line)
+	{
+		char **row = ft_split(line, ' ');
+		int i = 0;
+		while (row[i])
+		{
+			ft_is
+		}
+	}
+}
+
+
+/*
+
 void	my_mlx_pixel_put(t_image *data, int x, int y, int color) {
 	char	*dst;
 
@@ -18,38 +62,23 @@ int	render(t_mlx_data *data)
 	return (mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img.img, 0, 0));
 }
 
-int	main(int varc, char **argv) {
-	t_image		img;
-	t_mlx_data	mlx;
-	int			fd;
-	char		*line;
+mlx.mlx_ptr = mlx_init();
+mlx.win_ptr = mlx_new_window(mlx.mlx_ptr, 500, 500, "Imagine");
+img.img = mlx_new_image(mlx.mlx_ptr, 500, 500);
+img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel,
+	&img.line_length,&img.endian);
+mlx.img = img;
 
-	if (varc != 2)
-		return (0);
-	fd = open(argv[1], O_RDONLY);
-	line = get_next_line_single(fd);
-	while (line)
+int x_start = 225;
+int y_start = 225;
+int size = 50;
+for (int y = y_start; y < y_start + size; y++)
+{
+	for (int x = x_start; x < x_start + size; x++)
 	{
-		char **tiles = ft_split(line, ' ');
-		
+		my_mlx_pixel_put(&mlx.img, x, y, 0x00FF0000);
 	}
-	mlx.mlx_ptr = mlx_init();
-	mlx.win_ptr = mlx_new_window(mlx.mlx_ptr, 500, 500, "Imagine");
-	img.img = mlx_new_image(mlx.mlx_ptr, 500, 500);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel,
-		&img.line_length,&img.endian);
-	mlx.img = img;
-	
-	int x_start = 225;
-	int y_start = 225;
-	int size = 50;
-	for (int y = y_start; y < y_start + size; y++)
-	{
-		for (int x = x_start; x < x_start + size; x++)
-		{
-			my_mlx_pixel_put(&mlx.img, x, y, 0x00FF0000);
-		}
-	}
-	render(&mlx);
-	mlx_loop(mlx.mlx_ptr);
 }
+render(&mlx);
+mlx_loop(mlx.mlx_ptr);
+*/
--- fdf.h
+++ fdf.h
@@ -6,17 +6,12 @@
 /*   By: asadik <asadik@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/02 15:13:20 by asadik            #+#    #+#             */
-/*   Updated: 2026/02/02 15:35:32 by asadik           ###   ########.fr       */
+/*   Updated: 2026/02/09 13:53:49 by asadik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FDF_H
 # define FDF_H
-
-typedef struct	s_tile {
-	int	height;
-	int	color;
-}				t_tile;
 
 typedef struct	s_image {
 	void	*img;
@@ -32,9 +27,21 @@ typedef struct	s_mlx_data {
 	t_image	img;
 }				t_mlx_data;
 
-typedef struct	s_map {
+typedef struct	s_point {
+	int	height;
+	int	color;
+}				t_point;
+
+typedef struct	s_world {
 	int		width;
 	int		height;
-	t_tile	*tiles;
-}				t_map;
+	t_point	*points;
+}				t_world;
+
+typedef struct	s_state {
+	t_image		image;
+	t_mlx_data	mlx;
+	t_world		world;
+}				t_state;
+
 #endif
