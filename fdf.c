e05b7415f04aee3390d2e4f85313ea6af38d597b
Anton Sadik <asadik@c3r11p8.42vienna.com>
2026-02-11 15:28:59 +0100

    Basic reading and parsing done,  fixed up some norminette errors

--- fdf.c
+++ fdf.c
@@ -1,52 +1,127 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asadik <asadik@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/11 15:22:22 by asadik            #+#    #+#             */
+/*   Updated: 2026/02/11 15:27:03 by asadik           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <mlx.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 
-#include "libft/get_next_line/get_next_line.h"
 #include "libft/libft.h"
 #include "fdf.h"
 
-int	setup_world(const char *file, t_world *world)
+int	ft_atoi_hex(void)
 {
-	int			fd;
-	char		*line;
-	
+	return (0);
+}
+
+void	prepare_world(char **rows, t_world *world)
+{
+	int		i;
+
+	i = 0;
+	world->width = ft_strlen(rows[0]);
+	while (rows[i])
+		i++;
+	world->height = i;
+	world->points = ft_calloc(world->width * world->height, sizeof(t_point));
+}
+
+void	parse_line(char **rows, int row_i, t_world *world)
+{
+	char	**row;
+	int		col_i;
+	char	**test;
+	int		index;
+
+	row = ft_split(rows[row_i], ' ');
+	col_i = 0;
+	while (row[col_i])
+	{
+		index = row_i * world->width + col_i;
+		if (ft_strchr(row[col_i], ','))
+		{
+			test = ft_split(row[col_i], ',');
+			world->points[index].height = ft_atoi(test[0]);
+			world->points[index].color = ft_atoi_hex();
+		}
+		else
+		{
+			world->points[index].height = ft_atoi(row[col_i]);
+			world->points[index].color = 0;
+		}
+		col_i++;
+	}
+}
+
+void	parse_map(t_world *world, char *map)
+{
+	char	**rows;
+	int		i;
+	int		e;
+	char	**row;
+
+	rows = ft_split(map, '\n');
+	prepare_world(rows, world);
+	i = 0;
+	while (rows[i])
+	{
+		parse_line(rows, i, world);
+		i++;
+	}
+}
+
+t_world	setup_world(const char *file)
+{
+	int		fd;
+	char	*new_line;
+	char	*map;
+	t_world	world;
+
+	map = ft_calloc(1, 1);
 	fd = open(file, O_RDONLY);
-	line = get_next_line_single(fd);
+	new_line = get_next_line_single(fd);
+	while (new_line)
+	{
+		map = ft_strjoin(map, new_line);
+		new_line = get_next_line_single(fd);
+	}
+	parse_map(&world, map);
+	return (world);
 }
 
 t_state	setup(const char *file)
 {
 	t_state		state;
+
 	state.mlx.mlx_ptr = mlx_init();
 	state.mlx.win_ptr = mlx_new_window(state.mlx.mlx_ptr, 500, 500, "FDF");
 	state.image.img = mlx_new_image(state.mlx.mlx_ptr, 500, 500);
-	state.image.addr = mlx_get_data_addr(state.image.img, &state.image.bits_per_pixel,
-		&state.image.line_length,&state.image.endian);
+	state.image.addr = mlx_get_data_addr(state.image.img,
+			&state.image.bits_per_pixel, &state.image.line_length,
+			&state.image.endian);
 	state.mlx.img = state.image;
-	setup_world(file, &state.world);
+	state.world = setup_world(file);
+	return (state);
 }
 
-int	main(int varc, char **argv) {
+int	main(int varc, char **argv)
+{
 	t_state		state;
 	t_image		img;
 	t_mlx_data	mlx;
 
-
 	if (varc != 2)
 		return (0);
 	state = setup(argv[1]);
-	while (line)
-	{
-		char **row = ft_split(line, ' ');
-		int i = 0;
-		while (row[i])
-		{
-			ft_is
-		}
-	}
 }
-
 
 /*
 
--- fdf.h
+++ fdf.h
@@ -6,14 +6,15 @@
 /*   By: asadik <asadik@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/02 15:13:20 by asadik            #+#    #+#             */
-/*   Updated: 2026/02/09 13:53:49 by asadik           ###   ########.fr       */
+/*   Updated: 2026/02/11 15:26:43 by asadik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FDF_H
 # define FDF_H
 
-typedef struct	s_image {
+typedef struct s_image
+{
 	void	*img;
 	char	*addr;
 	int		bits_per_pixel;
@@ -21,24 +22,28 @@ typedef struct	s_image {
 	int		endian;
 }				t_image;
 
-typedef struct	s_mlx_data {
+typedef struct s_mlx_data
+{
 	void	*mlx_ptr;
 	void	*win_ptr;
 	t_image	img;
 }				t_mlx_data;
 
-typedef struct	s_point {
+typedef struct s_point
+{
 	int	height;
 	int	color;
 }				t_point;
 
-typedef struct	s_world {
+typedef struct s_world
+{
 	int		width;
 	int		height;
 	t_point	*points;
 }				t_world;
 
-typedef struct	s_state {
+typedef struct s_state
+{
 	t_image		image;
 	t_mlx_data	mlx;
 	t_world		world;
